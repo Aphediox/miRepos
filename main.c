813af293702c31cadb2f8a808a1221c3f3999f14
Salvador Torrres <jorge.torres4847@alumnos.udg.mx>
2020-09-15 12:14:48 -0500

    Creamos una funcion para saludar

--- main.c
+++ main.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
+void saludo(char[]);
 int main(){
     char nombre [50];
     printf("Nombre: \n");
     fgets(nombre, sizeof(nombre), stdin);
-    printf("Hola %s", nombre);
+    saludo(nombre);
     return 0;
+}
+void saludo(char nombre[]){
+    
+    printf("Hola %s", nombre);
 }
